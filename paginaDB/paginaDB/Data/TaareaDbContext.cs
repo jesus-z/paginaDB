19662f31e46509604acaee7e30a46243dedd4bfc
jesus-z <jedu_1010@hotmail.com>
2025-08-26 21:00:46 -0400

    falta migracion

---
+++ paginaDB/paginaDB/Data/TaareaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using paginaDB.Models;
+
+namespace paginaDB.Data
+{
+    public class TareaDbContext : DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+        protected TareaDbContext()
+        {
+        }
+    }
+}
---
+++ paginaDB/paginaDB/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace paginaDB.Models
+{
+    public class Tarea
+    {
+        public int idTarea { get; set; }
+        public string nombreTarea { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string estado { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
